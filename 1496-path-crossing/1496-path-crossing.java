29f0c02e93d97d37860f8095a8964f4768e46ad1
Devanshi <devanshi.shishodia29@gmail.com>
2026-07-11 13:56:10 +0530

    Time: 6 ms (5.19%), Space: 43.4 MB (54.06%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -7,13 +7,12 @@ class Solution {
        for(int i=0;i<path.length();i++)
        {
         char ch=path.charAt(i);
-         if(ch =='N') y+=1;
-         else if(ch == 'W')x-=1;
-         else if(ch == 'E') x+=1;
-         else if(ch == 'S')y-=1;
-         String temp= x + "," + y;
-        if(hs.contains(temp)) return true;
-        hs.add(temp);
+         if(ch =='N') y++;
+         else if(ch == 'W')x--;
+         else if(ch == 'E') x++;
+         else if(ch == 'S')y--;
+        if(hs.contains(x + "," + y)) return true;
+        hs.add(x + "," + y);
          
        }
 
